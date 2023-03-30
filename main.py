00da0fa4c6eca44ee99865d56946d58b1a04cc04
Jacob Hein <jacobhein1892@gmail.com>
2023-03-30 15:13:59 -0700

    able to choose a filter column

--- main.py
+++ main.py
@@ -7,106 +7,129 @@
 # Second iteration, create CLI for interaction with program
 import csv
 import pandas
-import click
 import curses
 
 
 class File:
+
     def __init__(self, name, finalName):
         self.name = name # The file name that we are reading from
         self.final = finalName # What we are going to name the file once we've completed the filter
+        self.readFile()
 
     def description(self):
         print(f"We are reading from {self.name}, and turning it into {self.final}")
 
     def readFile(self):
         df = pandas.read_csv(self.name)
-        nullDf = df.dropna() # All rows with a Nan value are being dropped
-        nullDf.to_csv(self.final, index = False) # create and write a new file with the updated files.
+        self.df = df
+        self.getColumns()
+    
+        # nullDf = df.dropna() # All rows with a Nan value are being dropped
+        # nullDf.to_csv(self.final, index = False) # create and write a new file with the updated files.
+
+    # Get the columns for this file
+    def getColumns(self): #
+        self.column_names = self.df.columns
+        self.setFilterParams()
+        
+    
+    def setColumnFilter(self):
+        print('setColumnFilter')
+        index = 0 # So we know where we are in the list
+        chosen = False
+
+        # Initialize the curses library
+        stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+        stdscr.keypad(True)
+        
+        
+        # Print the initial list of options
+        for i, option in enumerate(self.column_names):
+            print(option)
+            if i == index:
+                stdscr.addstr(f"> {option}\n", curses.A_STANDOUT)
+            else:
+                stdscr.addstr(f"  {option}\n")
+        
+
+        while chosen is False:
+            key = stdscr.getch()
+            
+            # Move the selected option up or down
+            if key == curses.KEY_UP:
+                index = (index - 1) % len(self.column_names)
+            elif key == curses.KEY_DOWN:
+                index = (index + 1) % len(self.column_names)
+            elif key == curses.KEY_ENTER or key == 10 or key == 13:
+                self.filterColumn = self.column_names[index]
+                chosen = True
+            
+            # Print the updated list of options
+            stdscr.clear()
+            for i, option in enumerate(self.column_names):
+                if i == index:
+                    stdscr.addstr(f"> {option}\n", curses.A_STANDOUT)
+                else:
+                    stdscr.addstr(f"  {option}\n")
+        
+        # Clean up the curses library
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+        
+
+    def setFilterParams(self):
+        print('In FilterParams')
+        moreFilters = True
+        # So this is going to start to build us our filtering parameters
+        # We want to first display all of our column names as options.
+        # I want this to be like building a query for sql, but we are going to build a pandas function.
+        while moreFilters:
+            self.setColumnFilter()
+            print(f"Your chosen Column is: {self.filterColumn}")
+            moreFilters = False
+        # First Pick column
+        
+        # Second Pick operation (<, >, =, <=, >=)
+
+        # Third Pick value (string or number)
+        
+
 
 
 
 add = True
 baseDirectory = ''
-options = ["1", "2", "3", "4"]
-
-def select_options():
-    selected_options = []
-    index = 0
-    
-    # Initialize the curses library
-    stdscr = curses.initscr()
-    curses.noecho()
-    curses.cbreak()
-    stdscr.keypad(True)
-    
-    # Print the initial list of options
-    for i, option in enumerate(options):
-        if i == index:
-            stdscr.addstr(f"> {option}\n")
-        else:
-            stdscr.addstr(f"  {option}\n")
-    
-    # Wait for user input
-    while len(selected_options) < 3:
-        key = stdscr.getch()
-        
-        # Move the selected option up or down
-        if key == curses.KEY_UP:
-            index = (index - 1) % len(options)
-        elif key == curses.KEY_DOWN:
-            index = (index + 1) % len(options)
-        elif key == curses.KEY_ENTER or key == 10 or key == 13:
-            selected_options.append(options[index])
-        
-        # Print the updated list of options
-        stdscr.clear()
-        for i, option in enumerate(options):
-            if i == index:
-                stdscr.addstr(f"> {option}\n")
-            else:
-                stdscr.addstr(f"  {option}\n")
-    
-    # Clean up the curses library
-    curses.nocbreak()
-    stdscr.keypad(False)
-    curses.echo()
-    curses.endwin()
-    
-    return selected_options
-
-# Call the select_options function and print the selected options
-selected = select_options()
-print(f"Selected options: {selected}")
-
-
-# while add is True:
 
 
 
 
 
-#     # print(baseDirectory)
-#     # if len(baseDirectory) < 1:
-#     #     baseDirectory = input("What is the absolute path to the base directory for your data files? ")
-#     # else:
-#     #     newDirectory = input(f"Is this file also found in: {baseDirectory}? (y/n) ")
-#     #     if newDirectory != 'y':
-#     #         baseDirectory = input("What is the absolute path to the base directory for your data files? ")
+while add is True:
 
-#     # name = input("What is the name of file you'd like to filter? ")
-#     # finalName = input("What would you like to name the file containing the filtered data? ")
-#     # fileName = baseDirectory + '/' + name
+    if len(baseDirectory) < 1:
+        baseDirectory = input("What is the absolute path to the base directory for your data files? ")
+    else:
+        newDirectory = input(f"Is this file also found in: {baseDirectory}? (y/n) ")
+        if newDirectory != 'y':
+            baseDirectory = input("What is the absolute path to the base directory for your data files? ")
 
-#     # newFile = File(fileName, finalName)
-#     # newFile.description()
-#     # # newFile.readFile()
-#     # userContinue = input("Would you like to filter another file? (y/n) ")
-#     # if userContinue !='y':
-#     #     add = False
+    name = input("What is the name of file you'd like to filter? ")
+    finalName = input("What would you like to name the file containing the filtered data? ")
+    fileName = baseDirectory + '/' + name
+
+    newFile = File(fileName, finalName) # Create new File
+
+    userContinue = input("Would you like to filter another file? (y/n) ")
+    if userContinue !='y':
+        add = False
 
 
-# print("Thank you for using our data processor!")
+print("Thank you for using our data processor!")
 
 
 
@@ -114,3 +137,60 @@ print(f"Selected options: {selected}")
 
     
 # /Users/jacobhein/Documents/MLBData/test.csv
+
+
+
+
+
+#### THIS IS FOR THE COMMAND LINE INTEREACTION ####
+# options = ["1", "2", "3", "4"]
+
+# def select_options():
+#     selected_options = []
+#     index = 0
+    
+#     # Initialize the curses library
+#     stdscr = curses.initscr()
+#     curses.noecho()
+#     curses.cbreak()
+#     stdscr.keypad(True)
+    
+#     # Print the initial list of options
+#     for i, option in enumerate(options):
+#         if i == index:
+#             stdscr.addstr(f"> {option}\n")
+#         else:
+#             stdscr.addstr(f"  {option}\n")
+    
+#     # Wait for user input
+#     while len(selected_options) < 3:
+#         key = stdscr.getch()
+        
+#         # Move the selected option up or down
+#         if key == curses.KEY_UP:
+#             index = (index - 1) % len(options)
+#         elif key == curses.KEY_DOWN:
+#             index = (index + 1) % len(options)
+#         elif key == curses.KEY_ENTER or key == 10 or key == 13:
+#             selected_options.append(options[index])
+        
+#         # Print the updated list of options
+#         stdscr.clear()
+#         for i, option in enumerate(options):
+#             if i == index:
+#                 stdscr.addstr(f"> {option}\n")
+#             else:
+#                 stdscr.addstr(f"  {option}\n")
+    
+#     # Clean up the curses library
+#     curses.nocbreak()
+#     stdscr.keypad(False)
+#     curses.echo()
+#     curses.endwin()
+    
+#     return selected_options
+
+# # Call the select_options function and print the selected options
+# selected = select_options()
+# print(f"Selected options: {selected}")
+####### #############
